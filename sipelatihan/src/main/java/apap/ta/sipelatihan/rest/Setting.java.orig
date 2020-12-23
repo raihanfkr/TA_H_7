0c8ba4c9fde70d1ebdf87624d620ddd63bf3f96d
Khansa Khairunisa Putri <khansa.khairunisa@ui.ac.id>
2020-12-23 23:43:31 +0700

    new updated project

--- sipelatihan/src/main/java/apap/ta/sipelatihan/rest/Setting.java.orig
+++
@@ -1,5 +0,0 @@
-package apap.ta.sipelatihan.rest;
-
-public class Setting {
-    final public static String APIsipegawai = "https://si-pegawai.herokuapp.com";
-}
--- sipelatihan/src/main/java/apap/ta/sipelatihan/service/UserRestService.java
+++
@@ -1,9 +0,0 @@
-package apap.ta.sipelatihan.service;
-
-import apap.ta.sipelatihan.rest.BaseResponse;
-import apap.ta.sipelatihan.rest.PegawaiDTO;
-
-public interface UserRestService {
-    BaseResponse addPegawai(PegawaiDTO pegawai);
-    BaseResponse getPegawai(String username);
-}
